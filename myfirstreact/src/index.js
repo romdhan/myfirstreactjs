5666bd009b132b90f6fa193642395af35cef157a
ROMDHAN FRIKHA <roma_frikha@hotmail.com>
2021-09-14 23:48:00 +0200

    - test components & classes

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+class Car2 extends React.Component {
+  constructor() {
+    super();
+    this.state = {color: "red"};
+  }
+  render() {
+    return <h2>I am a {this.state.color} Car!</h2>;
+  }
+}
+
+class Car3 extends React.Component {
+  render() {
+    return <h2>I am a {this.props.color} Car!</h2>;
+  }
+}
+
+function CarClass() {
+  return <h2>Hi, I am also a Car!</h2>;
+}
+
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const myfirstelement = <h1>Hello React!</h1>
+
+// ReactDOM.render(<Car2 />, document.getElementById('root'));
+ReactDOM.render(<Car3 color="green"/>, document.getElementById('root'));
